e116ffbf9c92e8c3463e57a4f1e615639193d496
Grigorii K. Shartsev <me@shgk.me>
2020-06-25 21:21:27 +0500

    Add 03 final

---
+++ 03-VueCLI/01-Webpack/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <button>Обычная кнопка</button>
+    <alert-button text="Alert text">
+      <app-text text="Alert" />
+      Button
+    </alert-button>
+  </div>
+</template>
+
+<script>
+import AlertButton from "./components/AlertButton.vue";
+import AppText from "./components/AppText.vue";
+
+export default {
+  name: "App.vue",
+  components: {AppText, AlertButton },
+}
+</script>
+
+<style scoped>
+</style>
---
+++ 03-VueCLI/01-Webpack/src/components/AlertButton.vue
@@ -0,0 +1,59 @@
+<template>
+  <button @click="makeAlert">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "AlertButton",
+
+  props: ['text'],
+
+  methods: {
+    makeAlert() {
+      alert(this.text);
+    },
+  },
+
+};
+</script>
+
+<style scoped>
+
+button {
+  display: inline-block;
+  padding: 10px 24px;
+  font-weight: 700;
+  font-size: 20px;
+  line-height: 28px;
+  color: #4c6bb6;
+  text-align: center;
+  border: 4px solid #4c6bb6;
+  transition: .2s all;
+  outline: none;
+  box-shadow: none;
+  background-color: #fff;
+  cursor: pointer;
+  text-decoration: none;
+}
+
+button:hover {
+  border-color: #b8c5e2;
+}
+
+/* Работает, так как scoped стили действуют на корневой элемент дочерних компонентов */
+button .root{
+  font-style: italic;
+}
+
+/* Не работает, scoped стили не действуют на содержимое других компонентов */
+button .deep{
+  text-decoration: underline;
+}
+
+/* Работает, используется >>> */
+button >>> .deep{
+  text-transform: uppercase;
+}
+</style>
