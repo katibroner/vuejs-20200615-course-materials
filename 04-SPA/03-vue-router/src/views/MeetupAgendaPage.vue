e013ae5bab899770e1299f52d446d8d1e3f8df32
Grigorii K. Shartsev <me@shgk.me>
2020-06-29 21:42:36 +0500

    Add 04 final

---
+++ 04-SPA/03-vue-router/src/views/MeetupAgendaPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    {{ meetup.agenda.length }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MeetupAgenda',
+
+  props: {
+    meetup: Object,
+  },
+};
+</script>
+
+<style scoped></style>
